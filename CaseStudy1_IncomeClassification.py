74b40fece142e52991a3afac67c6c8696feb46f4
Grizzlyrj <166296702+Grizzlyrj@users.noreply.github.com>
2024-05-09 23:00:27 +0530

    Missing data values and crosstab
    found and dropped missing values, crosstab for relations and made a salary status bar plot

--- CaseStudy1_IncomeClassification.py
+++ CaseStudy1_IncomeClassification.py
@@ -51,3 +51,106 @@ data_bkup.isnull() # true shows missing data and false shows no missing data
 
 print("Data values with null values is:\n", data_bkup.isnull().sum())
 # therefore no missing values in any column
+
+#Getting summary of numerical columns
+
+print(data_bkup.describe())
+
+summary_numdata=data_bkup.describe() 
+
+#Getting summary of categorical columns
+
+summary_catdata=data_bkup.describe(include="O") #here O represents object data type
+print(summary_catdata)
+
+# Frequency of each category
+
+data_bkup['JobType'].value_counts() # there is ? in jobtype
+data_bkup['occupation'].value_counts() # there is ? in occupation
+data_bkup['nativecountry'].value_counts()
+data_bkup['age'].value_counts()
+data_bkup['capitalgain'].value_counts() #capital gain is the profit from 
+                                        #investments gained by selling-purchase price  
+
+data_bkup['capitalloss'].value_counts()#capital loss is the loss from 
+                                        #investments calculated by selling-purchase price 
+                                        
+# Checking for unique values
+
+print(np.unique(data_bkup['JobType']))
+print(np.unique(data_bkup['occupation']))
+#**** There exists ' ?' instesd of nan
+
+"""
+Go back and read the data by including "na_values[' ?']" to consider ' ?' as nan !!!
+"""
+
+data_bkup = pd.read_csv('income.csv',na_values=[" ?"]) 
+
+# =============================================================================
+# Data pre-processing
+# =============================================================================
+data_bkup.isnull().sum()
+
+"""
+# Here we know that there are 1809 missing values from Jobtype and 1816 missing from occupation
+We need to know how many rows are missing both column values. So we create a subset named 'missing'.
+"""
+
+missing=data_bkup[data_bkup.isnull().any(axis=1)] # axis=1 => to consider at least one column value is missing in a row
+
+""" Points to note:
+1. Missing values in Jobtype    = 1809
+2. Missing values in Occupation = 1816 
+3. There are 1809 rows where two specific 
+   columns i.e. occupation & JobType have missing values
+4. (1816-1809) = 7 => You still have occupation unfilled for 
+   these 7 rows. Because, jobtype is Never worked
+"""
+
+data_com=data_bkup.dropna(axis=0) #Created new df that dropped all rows with nan and contains only complete values.
+
+# Relationship between independent variables i.e numerical variables
+
+correlation=data_com.corr()
+
+print(data_com.dtypes)
+
+numeric_data_com=data_com.select_dtypes(include=['float','int'])
+
+correlation=numeric_data_com.corr()
+"""
+Upon checking correlation df, none of the combinations are closer to neither 1 nor -1. 
+This shows that there is no strong correlation btw variables.
+
+"""
+# =============================================================================
+# Cross tables & Data Visualization
+# =============================================================================
+# Extracting the column names
+
+data_com.columns
+
+
+# =============================================================================
+# Gender proportion table:
+# =============================================================================
+
+gender=pd.crosstab(data_com["gender"],columns='count',normalize=True) # normalize helps in getting percent values
+print(gender)
+
+# =============================================================================
+#  Gender vs Salary Status:
+# =============================================================================
+
+gender_salstat=pd.crosstab(data_com["gender"], columns=data_com["SalStat"], normalize=True, margins=True)
+print(gender_salstat)
+
+# =============================================================================
+# Frequency distribution of 'Salary status' 
+# =============================================================================
+SalStat = sns.countplot(x='SalStat', data=data_com)
+
+"""  75 % of people's salary status is <=50,000 
+     & 25% of people's salary status is > 50,000
+"""
